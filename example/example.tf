5b3a0824c90ab59b576387f2ed86f080ebd170a1
kamlesh <thekamleshyadav26@gmail.com>
2021-11-19 15:57:41 +0530

    inital commit

---
+++ example/example.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+module "aws_vpc" {
+      source = "../"
+      tag = {
+         createdby = "ajit"
+      }
+      enabled = true
+ subnet_enabled = true
+}
---
+++ example/output.tf
@@ -0,0 +1,7 @@
+output "vpc_id" {
+      value = module.aws_vpc.id
+}
+output "vpc_name" {
+      value = module.aws_vpc.name
+}
+
